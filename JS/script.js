69f81314b9cd9fd1ca843770d28e33cc010e6633
shahboznur <shahbozkarimovich9772@gmail.com>
2022-07-28 14:29:20 +0500

    JS first lesson

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+console.log(alert ('Xush kelibsiz!'));
+
+console.log(prompt ('Ismingiz kiriting?'));
+
+console.log(prompt ('Familyangiz kiriting?'));
+
+console.log(prompt ('Tug\'ilgan yilingi kiriting?'));
+
+console.log(prompt ('Yoshingiz kiriting?'));
+
+console.log(prompt ('Telefon raqamingiz kiriting?'));
+
+console.log(prompt ('Emailingizni kiriting?'));
+
+console.log(prompt ('Manzilingiz kiriting'));
+
+console.log(alert ('Malumotlaringizni kiritganingiz uchun raxmat!'));
